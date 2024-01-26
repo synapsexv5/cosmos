7aa7170ed50cb41d23db65800ef5613ed8c94832
Jade <110680472+YourBloxJade@users.noreply.github.com>
2024-01-26 13:18:44 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,26 +10,15 @@ self.addEventListener('message', (event) => {
 async function openWebsite(url) {
   const webserverIP = ''; // IP address of the controlling webserver
   const modifiedRequest = new Request(url.replace(/^https?:\/\//, 'http://' + webserverIP + '/'));
-  
+
   const response = await fetch(modifiedRequest);
 
   // Get the iframe element from the client page
-  self.clients.matchAll().then((clients) => {
+  self.clients.matchAll({ type: 'nested', includeUncontrolled: true }).then((clients) => {
     clients.forEach((client) => {
       if (client.frameType === 'nested' && client.frameId !== '') {
-        client.postMessage({ action: 'updateIframe', response: response.clone().text(), url });
+        client.postMessage({ action: 'updateIframe', response, url }); // sending response object directly
       }
     });
   });
-}
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetchAndModify(event.request));
-});
-
-async function fetchAndModify(request) {
-  const webserverIP = ''; // IP address of the controlling webserver
-  const modifiedRequest = new Request(request.url.replace(/^https?:\/\//, 'http://' + webserverIP + '/'));
-
-  return fetch(modifiedRequest);
 }
