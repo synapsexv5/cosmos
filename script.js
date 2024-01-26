95a9d5e07a9e7c23d27059c0fc09871d64284df6
Jade <110680472+YourBloxJade@users.noreply.github.com>
2024-01-26 13:10:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ document.getElementById('browseBtn').addEventListener('click', () => {
     openWebsite(url);
   }
 });
+
+function openWebsite(url) {
+  // Send message to service worker
+  navigator.serviceWorker.controller.postMessage({ action: 'openWebsite', url });
+}
+
+// Register service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+    .then(() => {
+      console.log('Service worker registered');
+    })
+    .catch((error) => {
+      console.error('Error registering service worker:', error);
+    });
+}
